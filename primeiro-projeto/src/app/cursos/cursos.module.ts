a311c422a7619bae80906573d76c125c8a29356e
prmorais13 <prmorais_13@hotmail.com>
2017-06-09 19:03:11 -0300

    Terceiro commit trabalho

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CursosComponent } from './cursos.component';
+import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    CursosComponent
+    CursosComponent,
+    CursosDetalheComponent
   ],
   exports: [
     CursosComponent
