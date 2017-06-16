5650d121a629b74564e03dbf6a1a7b20439e9cf1
prmorais13 <prmorais_13@hotmail.com>
2017-06-16 14:15:57 -0300

    Exercicio aula 15

--- data-bindig/src/app/app.module.ts
+++ data-bindig/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DataBindigComponent } from './data-bindig/data-bindig.component';
 import { InputPropertyComponent } from './input-property/input-property.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindigComponent,
     InputPropertyComponent,
-    OutputPropertyComponent
+    OutputPropertyComponent,
+    CicloComponent
   ],
   imports: [
     BrowserModule,
---
+++ data-bindig/src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnInit {
+
+  @Input() valorInicial: number = 10;
+
+  constructor() {
+    this.log('Constructor');
+  }
+
+  ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  ngOnChanges(){
+    this.log('ngOnChanges');
+  }
+
+  ngDoCheck(){
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(){
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(){
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(){
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(){
+    this.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(){
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string){
+    console.log(hook);
+  }
+}
